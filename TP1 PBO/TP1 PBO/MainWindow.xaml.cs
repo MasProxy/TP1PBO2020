6411c096d1bc33c439365347b581ecf00ab49ebd
Mas Proxy <afifalfauzan65@gmail.com>
2020-03-03 14:39:01 +0700

    update metode baru(make wpf)

---
+++ TP1 PBO/TP1 PBO/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP1_PBO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            if ((text_Username.Text=="MasProxy")&&(text_Password.Text== "pbo123")) {
+                Window1 Menubeli = new Window1();
+                Menubeli.Show();
+                this.Close();
+            }
+        }
+    }
+}
